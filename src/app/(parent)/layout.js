9fbbb47f54ca709bd3ca83ca87b0fff062da960a
Jamie-Glitch102 <schjam014@myuct.ac.za>
2026-08-15 14:17:03 +0200

    Implement parent dashboard and sidebar layout

---
+++ src/app/(parent)/layout.js
@@ -0,0 +1,13 @@
+import ParentSidebar from "@/components/parent/ParentSidebar";
+
+export default function ParentLayout({ children }) {
+  return (
+    <div className="flex min-h-screen">
+      <ParentSidebar />
+
+      <main className="ml-[240px] flex-1 bg-off-white p-8">
+        {children}
+      </main>
+    </div>
+  );
+}
