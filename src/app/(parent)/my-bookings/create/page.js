8336a63b3d60a2f95e7a167b30626ab114c314db
tashmacmay <tash.macmay@gmail.com>
2026-08-18 18:57:44 +0200

    Fix: padding and white text (css safety)

--- src/app/(parent)/my-bookings/create/page.js
+++ src/app/(parent)/my-bookings/create/page.js
@@ -1,10 +1,10 @@
-"use client";
+"use client"; //Allows interaction with UI (the client)
 
-export default function CreateBookingPage() {
+export default function CreateBookingPage() { //Function to return the UI components
   return (<>
-    <div className="flex justify-center gap-2.5 bg-[#08155A]"> {/*Safety net if bookings page refuses to connect:*/}
+    <div className="flex justify-center gap-2.5 bg-[#08155A] p-3 text-white"> {/*Safety net div banner in case bookings page refuses to connect:*/}
       <p>Having trouble viewing the calendar?</p>
-      <a href="https://bookings.cloud.microsoft/book/HidayatulIslamCollegeBookings@abcosystems365.onmicrosoft.com/?ismsaljsauthenabled"
+      <a href="https://outlook.office.com/book/HidayatulIslamCollegeBookings@abcosystems365.onmicrosoft.com/?ismsaljsauthenabled"
           target="_blank" 
           rel="noopener noreferrer" 
           className="btn-book-external text-[#ECC33B] hover:text-[#BA8E00]">
@@ -12,9 +12,9 @@ export default function CreateBookingPage() {
       </a> {/*target blank - tells the href to open in separate tab, rel  noopener noreferrer - security standard so wepapp is not recognised as the referrer to new tab and destination may not spoof opener https://stackoverflow.com/questions/57628890/why-people-use-rel-noopener-noreferrer-instead-of-just-rel-noreferrer*/}
     </div>
     <iframe
-      src="https://bookings.cloud.microsoft/book/HidayatulIslamCollegeBookings@abcosystems365.onmicrosoft.com/?ismsaljsauthenabled"
+      src="https://outlook.office.com/book/HidayatulIslamCollegeBookings@abcosystems365.onmicrosoft.com/?ismsaljsauthenabled"
       className="w-full h-[800px] border-0"
       title="Hidayatul Islam College Bookings"
-    />
+    /> {/*iframe embed link from Microsoft bookings edited for React's JSX rather than HTML e.g. class -> className*/}
   </>);
 }
