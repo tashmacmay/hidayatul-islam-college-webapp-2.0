5ce58432af26c26763843544125d6bb67b411f79
tashmacmay <tash.macmay@gmail.com>
2026-08-19 12:49:25 +0200

    chore: route and graphHelper cleanup and comments

--- src/app/api/bookings/route.js
+++ src/app/api/bookings/route.js
@@ -1,76 +1,27 @@
 //Adapting index.js from https://learn.microsoft.com/en-us/graph/tutorials/javascript-app-only to Next.js webapp
-//import { keyInSelect } from 'readline-sync';
-//import settings from './appSettings.js';
 import { NextResponse } from 'next/server';
 
-import {
+import { //Importing "export"ed functions from the graphHelper
   initializeGraphForAppOnlyAuth,
   getBookingBusinessesAsync,
-  getAppOnlyTokenAsync,
-  //getUsersAsync,
-  //makeGraphCallAsync,
 } from '@/lib/graph/graphHelper';
-
-// async function main() {
-//   console.log('JavaScript Graph App-Only Tutorial');
-
-//   let choice = 0;
-
-//   // Initialize Graph
-//   initializeGraph(settings);
-
-//   const choices = ['Display access token', 'List users', 'Make a Graph call'];
-
-//   while (choice != -1) {
-//     choice = keyInSelect(choices, 'Select an option', { cancel: 'Exit' });
-
-//     switch (choice) {
-//       case -1:
-//         // Exit
-//         console.log('Goodbye...');
-//         break;
-//       case 0:
-//         // Display access token
-//         await displayAccessTokenAsync();
-//         break;
-//       case 1:
-//         // List emails from user's inbox
-//         await listUsersAsync();
-//         break;
-//       case 2:
-//         // Run any Graph code
-//         await doGraphCallAsync();
-//         break;
-//       default:
-//         console.log('Invalid choice! Please try again.');
-//     }
-//   }
-// }
-
-//main();
-
-// function initializeGraph(settings) {
-//   initializeGraphForAppOnlyAuth(settings); //creates a new instance of ClientSecretCredential, then uses that instance to create a new instance of Client
-// // }
-// import {
-//     initializeGraphForOnlyAuth
-// } from '@/lib/graph/graphHelper.js';
 
 export async function GET() {
   try {
-    //initializeGraphForAppOnlyAuth(); //IGNORED TILL MOCK BOOKING DATA IS DROPPED
+    //IGNORED till delegated API permissions fixed:
+    //initializeGraphForAppOnlyAuth(); //Calls to initialises graphHelper
+    
+    //const token = await getAppOnlyTokenAsync();
+    //console.log('Token acquired successfully');
+    //console.log('Token length:', token?.length);
 
-    const token = await getAppOnlyTokenAsync();
-
-    console.log('Token acquired successfully');
-    console.log('Token length:', token?.length);
-
-    //const bookingBusinesses = await getBookingBusinessesAsync(); //IGNORED TILL MOCK BOOKING DATA IS DROPPED
-    const bookings = mockBookings;
-    //MOCK DATA FILLING IN ABOVE ^
-    //when graph api permission granted:
-    //await saveBookings(bookings);
-    return NextResponse.json(bookingBusinesses);
+    //IGNORED till delegated API permissions fixed:
+    //const bookingBusinesses = await getBookingBusinessesAsync(); //Calls graphHelper to return MS Bookings data
+    const bookings = mockBookings; //^ TEMP REPLACEMENT for getBooking... - DO NOT treat as official shape of booking data returned (only mock data)
+    
+    //IGNORED till delegated API permissions fixed:
+    //await saveBookings(bookings); //Actual booking data
+    return NextResponse.json(bookingBusinesses); //In response to GET(), return the data through Next.js as JSON
 
   } catch (error) {
     console.error('FULL GRAPH ERROR:', error);
@@ -87,20 +38,3 @@ export async function GET() {
     );
   }
 }
-
-// async function displayAccessTokenAsync() {
-//   try {
-//     const appOnlyToken = await getAppOnlyTokenAsync();
-//     console.log(`App-only token: ${appOnlyToken}`);
-//   } catch (err) {
-// //     console.log(`Error getting app-only access token: ${err}`);
-// //   }
-// // }
-
-// async function listUsersAsync() {
-//   // TODO
-// }
-
-// async function doGraphCallAsync() {
-//   // TODO
-// }
--- src/lib/graph/graphHelper.js
+++ src/lib/graph/graphHelper.js
@@ -1,6 +1,5 @@
 //File created with https://learn.microsoft.com/en-us/graph/tutorials/javascript-app-only
 //And adapted for Next.js
-//module.exports = {};
 
 import 'isomorphic-fetch';
 import { ClientSecretCredential } from '@azure/identity';
@@ -9,71 +8,41 @@ import { Client } from '@microsoft/microsoft-graph-client';
 import { TokenCredentialAuthenticationProvider } from
   '@microsoft/microsoft-graph-client/authProviders/azureTokenCredentials/index.js';
 
-//declares two private properties, a ClientSecretCredential object and a Client object:
-//let _settings = undefined;
+//Declares two private properties for initialising Graph: ClientSecretCredential object & Client object (outside of the initializeGraphForAppOnlyAuth() function as to persist)
 let _clientSecretCredential = undefined;
 let _appClient = undefined;
 
-//creates the ClientSecretCredential, then creates the Graph client using that credential
+//Creates the Client Secret Credential, then creates the Microsoft Graph Client using that credential
 export function initializeGraphForAppOnlyAuth() {
-  // Ensure settings isn't null
-  // if (!settings) {
-  //   throw new Error('Settings cannot be undefined');
-  // }
-
-  // _settings = settings;
-
-  // // Ensure settings isn't null
-  // if (!_settings) {
-  //   throw new Error('Settings cannot be undefined');
-  // }
-
-  if (!_clientSecretCredential) {
-    _clientSecretCredential = new ClientSecretCredential(
-      // _settings.tenantId,
-      // _settings.clientId,
-      // _settings.clientSecret,
+  if (!_clientSecretCredential) { //Is there already a Client Secret Credential? If not:
+    _clientSecretCredential = new ClientSecretCredential( //Make a new one! Using our secret IDs from .env.local
       process.env.MS_TENANT_ID,
       process.env.MS_CLIENT_ID,
       process.env.MS_CLIENT_SECRET
     );
   }
 
-  if (!_appClient) {
-    const authProvider = new TokenCredentialAuthenticationProvider(
-      _clientSecretCredential,
+  if (!_appClient) { //Is there already an App Client? If not: 
+    const authProvider = new TokenCredentialAuthenticationProvider( //Create an Authentication Provider, using:
+      _clientSecretCredential, //Client Secret Credentials created above this block ^
       {
         scopes: ['https://graph.microsoft.com/.default'],
       },
     );
 
-    _appClient = Client.initWithMiddleware({
+    _appClient = Client.initWithMiddleware({ //Creates actual Microsoft Graph Client using ^ Auth Provider (with our Client Secret Credentials)
       authProvider: authProvider,
     });
   }
 }
 
-//Code to get an access token from the ClientSecretCredential: (only useful for testing/debugging)
-export async function getAppOnlyTokenAsync() {
-  // Ensure credential isn't undefined
-  if (!_clientSecretCredential) {
-    throw new Error('Graph has not been initialized for app-only auth');
-  }
-
-  // Request token with given scopes 
-  const response = await _clientSecretCredential.getToken([
-    'https://graph.microsoft.com/.default',
-  ]);
-  return response.token;
-}
-
-// Gets the Microsoft Bookings businesses available to the application
+//Gets the Microsoft Bookings businesses available to the application
 export async function getBookingBusinessesAsync() {
-  if (!_appClient) {
+  if (!_appClient) { //Ensure that the Microsoft Graph Client has been created
     throw new Error('Graph has not been initialized for app-only auth');
   }
 
   return _appClient
-    .api('/solutions/bookingBusinesses')
-    .get();
+    .api('/solutions/bookingBusinesses') //Interact with Microsoft Graph endpoint
+    .get(); //Perform an HTTP GET request
 }
