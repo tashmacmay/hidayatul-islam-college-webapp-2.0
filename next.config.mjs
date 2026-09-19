65620343e18203b3f69b262797c10176002edadf
tashmacmay <tash.macmay@gmail.com>
2026-09-19 20:59:35 +0200

    Re-add COOP header fix (necessary but not sufficient on it's own to fix for Mac issue)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
